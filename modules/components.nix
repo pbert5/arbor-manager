b94a7822806d397c7018d508a3f6c3c0ecff3cec
ash-desktoptoodle <phsilbert@gmail.com>
2026-08-25 00:48:05 -0500

    security: bind delegated relationships to their issuer

--- modules/components.nix
+++ modules/components.nix
@@ -1,12 +1,16 @@
 { inputs, ... }:
 {
   # Export child component modules from one neutral composition boundary.
-  config.flake.homeModules = {
-    ashzsh = inputs.ashzsh.homeModules.default;
-    tilingDesktop = inputs.tilingDesktop.homeModules.default;
+  config.flake = {
+    homeModules = {
+      ashzsh = inputs.ashzsh.homeModules.default;
+      tilingDesktop = inputs.tilingDesktop.homeModules.default;
+    };
+    nixosModules = {
+      tilingDesktop = inputs.tilingDesktop.nixosModules.default;
+      arborRegistry = inputs.arbor-registry.nixosModules.default;
+      arborVaultRuntime = inputs.arbor-registry.nixosModules.vault-runtime;
+      arborVaultRuntimeUpstream = inputs.arbor-registry.nixosModules.vault-runtime-upstream;
+    };
   };
-  config.flake.nixosModules.tilingDesktop = inputs.tilingDesktop.nixosModules.default;
-  config.flake.nixosModules.arborRegistry = inputs.arbor-registry.nixosModules.default;
-  config.flake.nixosModules.arborVaultRuntime = inputs.arbor-registry.nixosModules.vault-runtime;
-  config.flake.nixosModules.arborVaultRuntimeUpstream = inputs.arbor-registry.nixosModules.vault-runtime-upstream;
 }
