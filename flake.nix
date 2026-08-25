2cb01b809eb26d69ab170fb7b1e8389ecfe8960e
ash-desktoptoodle <phsilbert@gmail.com>
2026-08-25 15:02:18 -0500

    fix: pin Ygg input through Git transport

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     arbor-manager.url = "github:pbert5/arbor-manager";
     arbor-registry.url = "github:pbert5/arbor-registry";
     arbor-network-manager.url = "github:pbert5/arbor-network-manager";
-    yggdrasil-private.url = "github:pbert5/yggdrasil-private";
+    yggdrasil-private.url = "git+https://github.com/pbert5/yggdrasil-private.git?ref=main";
   };
 
   outputs =
